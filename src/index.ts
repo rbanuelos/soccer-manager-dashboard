89b28ebe2a41bc06d5b8da61463c071180baf80e
rbanuelospy <robertobanuelos@gmail.com>
2022-05-11 20:34:43 -0400

    added router for /tactic path

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import express, { Express, Request, Response } from 'express'
+import express, { Express } from 'express'
 import dotenv from 'dotenv'
-import { Color, ElementArrow, ElementType, Tactic } from './model'
+import { tacticRouter } from './routes/tactic'
 
 dotenv.config()
 
@@ -11,47 +11,7 @@ if (port === undefined) {
   throw new Error('Invalid port value!')
 }
 
-app.get('/', (req: Request, res: Response) => {
-  const tactic: Tactic = {
-    id: 1,
-    elements: [{
-      id: 1,
-      name: 'Player 1',
-      position: {
-        x: 10,
-        y: 10
-      },
-      type: ElementType.PLAYER,
-      attributes: {
-        arrow: null,
-        color: Color.RED
-      }
-    },
-    {
-      id: 2,
-      name: 'Ball',
-      position: {
-        x: 20,
-        y: 10
-      },
-      type: ElementType.BALL
-    },
-    {
-      id: 3,
-      name: 'Player 2',
-      position: {
-        x: 30,
-        y: 30
-      },
-      type: ElementType.PLAYER,
-      attributes: {
-        arrow: ElementArrow.RIGHT,
-        color: Color.BLUE
-      }
-    }]
-  }
-  res.send(tactic)
-})
+app.use(tacticRouter)
 
 app.listen(port, () => {
   console.log(`⚡️[server] : Server is running at http://localhost:${port}`)
